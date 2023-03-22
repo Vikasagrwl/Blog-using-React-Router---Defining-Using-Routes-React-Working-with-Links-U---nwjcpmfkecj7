11b2d22d110aca734a7dbf75d3534d0a673a66fe
Vikasagrwl <105685053+Vikasagrwl@users.noreply.github.com>
2023-03-22 22:05:50 +0530

    Update PostDetail.js

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -2,5 +2,10 @@ import React from 'react';
 
 export default function PostDetail({ match }) {
   // Complete the function
+  // console.log(match)
+  return(
+    <div>Post Detail: {match.params.id}</div>
+  )
+
 }
   
